81ddbb92d9b65e6a2a89d774519a6a9e661b5220
AbnerLin <shaoyang0805@gmail.com>
2018-05-10 00:29:01 +0800

    cover not done.

--- src/components/Cover/index.js
+++ src/components/Cover/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-// import { connect } from 'react-redux';
 import { Col, Card, Button, CardTitle, CardText } from 'reactstrap';
+import { translate } from 'react-i18next';
 
 import './index.css';
-import head from './head.png';
+import * as Typicons from 'react-icons/lib/ti'
+
 
 class Cover extends React.Component {
 
@@ -18,37 +19,54 @@ class Cover extends React.Component {
   }
 
   render() {
+    const { t } = this.props;
+
     return (
       <div id="main" className="container-fluid">
         <div className="row">
-          <div className="col-md-4 d-flex justify-content-center">
-            <img src={head} alt="head" className="rounded-circle head " />
+          <div className="col-md-3 d-flex justify-content-center">
+            <img src="/images/head.jpg" alt="head" className="rounded-circle head" />
           </div>
-          <div className="col-md-4 d-flex justify-content-center">
-            <Card body outline color="primary">
-              <CardTitle>Special Title Treatment</CardTitle>
-              <CardText>
-                With supporting text below as a natural lead-in to additional content.
 
-              </CardText>
-              <Button color="secondary">Button</Button>
-            </Card>
-          </div>
-          <div className="col-md-4 d-flex justify-content-center">
-            <Card body outline color="primary">
-              <CardTitle>Special Title Treatment</CardTitle>
-              <CardText>
-                With supporting text below as a natural lead-in to additional content.
+          <div className="col-md-9 d-flex justify-content-center">
+            <div className="row w-100 d-flex align-items-center">
+              <Col md="5">
+                <Card body outline color="primary">
+                  <CardTitle>{ t("personal_info.title") }</CardTitle>
+                  <CardText>
+                    <Typicons.TiMail className="icon" />{ t("mail") }
+                    <br />
+                    { t("personal_info.mail") }<br />
+                    <Typicons.TiDevicePhone className="icon" />{ t("mobile") }
+                    <br />
+                    { t("personal_info.mobile") }<br />
+                    <Typicons.TiSocialLinkedin className="icon" />{ t("linkedin") }
+                    <br />
+                    { t("personal_info.linkedin") }
+                  </CardText>
+                </Card>
+              </Col>
 
-              </CardText>
-              <Button color="secondary">Button</Button>
-            </Card>
+              <Col md="7">
+                <Card body outline color="primary">
+                  <CardTitle>{ t("skill.title") }</CardTitle>
+                    <ul>
+                      {
+                        Object.values(t("skill.skill_list", { returnObjects: true })).map((value, index) =>
+                          <li key={index}>{value}</li>
+                        )
+                      }
+                    </ul>
+                </Card>
+              </Col>
+            </div>
           </div>
+
         </div>
       </div>
     )
   }
 }
 
-export default Cover;
+export default translate()(Cover);
 
