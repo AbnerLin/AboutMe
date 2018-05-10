2cb6587c9dcf8f6eada4ec316074c201ce2e3eb8
AbnerLin <shaoyang0805@gmail.com>
2018-05-10 16:36:12 +0800

    Add scroll effect.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { translate } from 'react-i18next';
+import { Button, Nav, NavItem, NavLink, TabPane, TabContent } from 'reactstrap';
 
 import Cover from '../Cover';
+import Resume from '../Resume';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -10,35 +12,116 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      activeTab : '1'
+    };
+
+    this.navOnClick = this.navOnClick.bind(this);
+    this.pageOnScroll = this.pageOnScroll.bind(this);
+    this.getElementInfo = this.getElementInfo.bind(this);
+    this.pageOnResize = this.pageOnResize.bind(this);
+    this.calcScrollEffectInfo = this.calcScrollEffectInfo.bind(this);
   }
 
   componentDidMount() {
-    // pass
+    window.addEventListener('scroll', this.pageOnScroll);
+    window.addEventListener('resize', this.pageOnResize);
+    this.calcScrollEffectInfo();
   }
+
+  calcScrollEffectInfo() {
+    var anchorDom = this.getElementInfo('middle');
+    var targetDom = this.getElementInfo('content');
+
+    this.targetScrollMaxDistance = targetDom.offsetTop - (anchorDom.offsetTop + anchorDom.height);
+  }
+
+  getElementInfo(id) {
+    var target = document.getElementById(id);
+    return {
+      element: target,
+      offsetTop: target.offsetTop,
+      height: target.clientHeight
+    }
+  }
+
+  pageOnResize(event) {
+    this.calcScrollEffectInfo();
+  }
+
+  pageOnScroll(event) {
+    var anchorDom = this.getElementInfo('middle');
+    var targetDom = this.getElementInfo('content');
+
+    var percentOfGap = window.pageYOffset / (anchorDom.offsetTop + anchorDom.height);
+
+    var targetMargin = this.targetScrollMaxDistance * percentOfGap;
+    if(targetMargin < 5) {
+      targetMargin = 5;
+    } else if(targetMargin > this.targetScrollMaxDistance) {
+      targetMargin = this.targetScrollMaxDistance;
+    }
+
+    targetDom.element.style.marginTop = targetMargin * -1 + 5 + "px";
+  }
+
+  navOnClick(activeTab) {
+    if (this.state.activeTab !== activeTab) {
+      this.setState({
+        activeTab: activeTab
+      });
+    }
+    console.log(activeTab);
+  }
+
 
   render() {
     const { t } = this.props;
 
     return (
       <div className="App">
+        <div className="w-100 position-absolute p-1">
+          <div className="d-flex justify-content-end">
+            <Button outline color="primary" className="mx-1"
+              onClick={() => this.props.changeLanguage('en-US')}>English
+            </Button>
+            <Button outline color="warning" className="mx-1"
+              onClick={() => this.props.changeLanguage('zh-TW')}>繁體中文
+            </Button>
+          </div>
+        </div>
 
-            { t("key") }
+        <Cover changeLanguage={this.props.changeLanguage} />
 
-            <button
-              onClick={() => this.props.changeLanguage('en-US')}>en-US
-            </button>
-            <button
-              onClick={() => this.props.changeLanguage('zh-TW')}>zh-TW
-            </button>
-            <Cover />
-
-            <header className="App-header">
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-
+        <div id="content" className="p-3 container-fluid">
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                  href="#"
+                  active={this.state.activeTab === '1'}
+                  onClick={() => this.navOnClick('1')} >
+                { t("resume.title") }
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                  href="#"
+                  active={this.state.activeTab === '2'}
+                  onClick={() => this.navOnClick('2')} >
+                { t("projects.title") }
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Resume />
+            </TabPane>
+            <TabPane tabId="2">
+              aaa
+            </TabPane>
+          </TabContent>
+        </div>
       </div>
     );
   }
--- src/components/Cover/index.js
+++ src/components/Cover/index.js
@@ -1,67 +1,58 @@
 import React from 'react';
-import { Col, Card, Button, CardTitle, CardText } from 'reactstrap';
+import { Col, Card, CardTitle, CardText } from 'reactstrap';
 import { translate } from 'react-i18next';
 
-import * as Typicons from 'react-icons/lib/ti'
+import * as Typicons from 'react-icons/lib/ti';
 import './index.css';
 
 
 class Cover extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    console.log(this.props.language);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // console.log(nextProps.language);
-  }
-
   render() {
     const { t } = this.props;
 
     return (
-      <div id="main" className="container-fluid p-3">
-        <div className="row">
-          <div className="col-md-3 d-flex justify-content-center head-block m-3 m-md-0">
-            <img src="/images/head_1.png" alt="head" className="rounded-circle head" />
-          </div>
+      <div id="main" className="d-flex align-items-center">
+        <div id="middle" className="container-fluid p-3">
+          <div className="row">
+            <div className="col-md-3 d-flex justify-content-center head-block m-3 m-md-0">
+              <img src="/images/head_2.jpg" alt="head" className="rounded-circle head" />
+            </div>
 
-          <div className="col-md-9 d-flex justify-content-center">
-            <div className="row w-100 d-flex align-items-center">
-              <Col md="5" className="m-3 m-md-0">
-                <Card body outline color="success">
-                  <CardTitle>{ t("personal_info.title") }</CardTitle>
-                  <CardText>
-                    <Typicons.TiMail className="icon" />{ t("mail") }
-                    <br />
-                    { t("personal_info.mail") }<br />
-                    <Typicons.TiDevicePhone className="icon" />{ t("mobile") }
-                    <br />
-                    { t("personal_info.mobile") }<br />
-                    <Typicons.TiSocialLinkedin className="icon" />{ t("linkedin") }
-                    <br />
-                    { t("personal_info.linkedin") }
-                  </CardText>
-                </Card>
-              </Col>
+            <div className="col-md-9 d-flex justify-content-center">
+              <div className="row w-100 d-flex align-items-center">
+                <Col md="5" className="m-2 m-md-0">
+                  <Card body>
+                    <CardTitle>{ t("personal_info.title") }</CardTitle>
+                    <CardText>
+                      <Typicons.TiMail className="icon" />{ t("mail") }
+                      <br />
+                      { t("personal_info.mail") }<br />
+                      <Typicons.TiDevicePhone className="icon" />{ t("mobile") }
+                      <br />
+                      { t("personal_info.mobile") }<br />
+                      <Typicons.TiSocialLinkedin className="icon" />{ t("linkedin") }
+                      <br />
+                      { t("personal_info.linkedin") }
+                    </CardText>
+                  </Card>
+                </Col>
 
-              <Col md="7" className="m-3 m-md-0">
-                <Card body outline color="success">
-                  <CardTitle>{ t("skill.title") }</CardTitle>
-                    <ul>
-                      {
-                        Object.values(t("skill.skill_list", { returnObjects: true })).map((value, index) =>
-                          <li key={index}>{value}</li>
-                        )
-                      }
-                    </ul>
-                </Card>
-              </Col>
+                <Col md="7" className="m-2 m-md-0">
+                  <Card body>
+                    <CardTitle>{ t("skill.title") }</CardTitle>
+                      <ul>
+                        {
+                          Object.values(t("skill.skill_list", { returnObjects: true })).map((value, index) =>
+                            <li key={index}>{value}</li>
+                          )
+                        }
+                      </ul>
+                  </Card>
+                </Col>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     )
